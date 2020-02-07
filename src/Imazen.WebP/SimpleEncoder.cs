fe206120a6eb8798491ec379e5d55075097fab57
hexx.one <5312542+hexxone@users.noreply.github.com>
2020-02-07 15:50:14 +0100

    Switch from .NET to Net-Standard and make it work

--- src/Imazen.WebP/SimpleEncoder.cs
+++ src/Imazen.WebP/SimpleEncoder.cs
@@ -1,16 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Runtime.InteropServices;
 using System.Drawing;
 using Imazen.WebP.Extern;
 using System.IO;
 
-namespace Imazen.WebP {
+namespace Imazen.WebP
+{
     /// <summary>
     /// Encodes Bitmap objects into WebP format
     /// </summary>
-    public class SimpleEncoder {
+    public class SimpleEncoder
+    {
         public SimpleEncoder() { }
 
         public static string GetEncoderVersion()
@@ -30,7 +30,8 @@ namespace Imazen.WebP {
         /// <param name="quality"></param>
         /// <param name="noAlpha"></param>
         [Obsolete]
-        public void Encode(Bitmap from, Stream to, float quality, bool noAlpha) {
+        public void Encode(Bitmap from, Stream to, float quality, bool noAlpha)
+        {
             Encode(from, to, quality);
         }
 
@@ -40,19 +41,24 @@ namespace Imazen.WebP {
         /// <param name="from"></param>
         /// <param name="to"></param>
         /// <param name="quality"></param>
-        public void Encode(Bitmap from, Stream to, float quality) {
+        public void Encode(Bitmap from, Stream to, float quality)
+        {
             IntPtr result;
             long length;
 
             Encode(from, quality, out result, out length);
-            try {
+            try
+            {
                 byte[] buffer = new byte[4096];
-                for (int i = 0; i < length; i += buffer.Length) {
+                for (int i = 0; i < length; i += buffer.Length)
+                {
                     int used = (int)Math.Min((int)buffer.Length, length - i);
                     Marshal.Copy((IntPtr)((long)result + i), buffer, 0, used);
                     to.Write(buffer, 0, used);
                 }
-            } finally {
+            }
+            finally
+            {
                 NativeMethods.WebPSafeFree(result);
             }
 
@@ -67,7 +73,8 @@ namespace Imazen.WebP {
         /// <param name="result"></param>
         /// <param name="length"></param>
         [Obsolete]
-        public void Encode(Bitmap b, float quality, bool noAlpha, out IntPtr result, out long length) {
+        public void Encode(Bitmap b, float quality, bool noAlpha, out IntPtr result, out long length)
+        {
             Encode(b, quality, out result, out length);
         }
 
@@ -78,23 +85,28 @@ namespace Imazen.WebP {
         /// <param name="quality"></param>
         /// <param name="result"></param>
         /// <param name="length"></param>
-        public void Encode(Bitmap b, float quality, out IntPtr result, out long length) {
+        public void Encode(Bitmap b, float quality, out IntPtr result, out long length)
+        {
             if (quality < -1) quality = -1;
             if (quality > 100) quality = 100;
             int w = b.Width;
             int h = b.Height;
             var bd = b.LockBits(new Rectangle(0, 0, w, h), System.Drawing.Imaging.ImageLockMode.ReadOnly, b.PixelFormat);
-            try {
+            try
+            {
                 result = IntPtr.Zero;
 
-                if (b.PixelFormat == System.Drawing.Imaging.PixelFormat.Format32bppArgb) {
+                if (b.PixelFormat == System.Drawing.Imaging.PixelFormat.Format32bppArgb)
+                {
                     if (quality == -1) length = (long)NativeMethods.WebPEncodeLosslessBGRA(bd.Scan0, w, h, bd.Stride, ref result);
                     else length = (long)NativeMethods.WebPEncodeBGRA(bd.Scan0, w, h, bd.Stride, quality, ref result);
-                }else if (b.PixelFormat == System.Drawing.Imaging.PixelFormat.Format24bppRgb)
+                }
+                else if (b.PixelFormat == System.Drawing.Imaging.PixelFormat.Format24bppRgb)
                 {
                     if (quality == -1) length = (long)NativeMethods.WebPEncodeLosslessBGR(bd.Scan0, w, h, bd.Stride, ref result);
                     else length = (long)NativeMethods.WebPEncodeBGR(bd.Scan0, w, h, bd.Stride, quality, ref result);
-                }else
+                }
+                else
                 {
                     using (Bitmap b2 = b.Clone(new Rectangle(0, 0, b.Width, b.Height), System.Drawing.Imaging.PixelFormat.Format32bppArgb))
                     {
@@ -102,7 +114,9 @@ namespace Imazen.WebP {
                     }
                 }
                 if (length == 0) throw new Exception("WebP encode failed!");
-            } finally {
+            }
+            finally
+            {
                 b.UnlockBits(bd);
             }
         }
